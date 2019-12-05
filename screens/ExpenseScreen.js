373fe413653fdbd69323394b9ac27d7981f6932c
jmigus2 <jmigus2@illinois.edu>
2019-12-05 11:43:50 -0600

    EXPENSE SCREEN

--- screens/ExpenseScreen.js
+++ screens/ExpenseScreen.js
@@ -28,12 +28,12 @@ export default class ExpenseScreen extends Component {
         return (
             <View style={styles.container}>
                 <Ionicons style={styles.close} size={48} name={Platform.OS == "ios"?"ios-close":"md-close"} onPress={()=>this.props.navigation.goBack()} />
-                <Text style= {{fontSize: 21,fontFamily: "space-mono",}}> {this.props.navigation.state.params.title}</Text>
-                <Text style= {{fontSize: 21,fontFamily: "space-mono",}}>{'$'+parseFloat(this.props.navigation.state.params.amount).toFixed(2)}</Text>
-                <Text>{this.props.navigation.state.params.category}</Text>
-                <Text>{this.props.navigation.state.params.user}</Text>
-                <Text>{new Date(this.props.navigation.state.params.date).toDateString()}</Text>
-                <Image style={{width: 400, height: 400}} source={{uri: `data:image/jpg;base64,${this.base64}`}} />
+                <Text style= {{fontSize: 60,fontFamily: "Comfortaa",textAlign: "center",}}> {this.props.navigation.state.params.title}</Text>
+                <Text style= {{fontSize: 45,fontFamily: "Comfortaa",textAlign: "center", marginTop: 15}}>{'$'+parseFloat(this.props.navigation.state.params.amount).toFixed(2)}</Text>
+                <Text style= {{fontSize: 30,fontFamily: "Comfortaa",textAlign: "center", marginTop: 15}}>{this.props.navigation.state.params.category}</Text>
+                <Text style= {{fontSize: .1,textAlign: "center"}}>{this.props.navigation.state.params.user}</Text>
+                <Text style= {{fontSize: 30,fontFamily: "Comfortaa",textAlign: "center", marginTop: 15}}>{new Date(this.props.navigation.state.params.date).toDateString()}</Text>
+                <Image style={{width: 400, height: 400,marginTop: 30, marginLeft: 7.5}} source={{uri: `data:image/jpg;base64,${this.base64}`}} />
             </View>
         )
     }
@@ -42,9 +42,12 @@ export default class ExpenseScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: "#F5F5F5"
+
     },
     close: {
-        marginTop: 32,
+        marginBottom: 10,
+        marginTop: 20,
         marginLeft: 16,
     },
     expenseAmount: {
